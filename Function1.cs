8f4d1dd10e5f09c6222f992c7eef4aa0e17bb6d6
njmalaza <junior.malaza@rysis.co.za>
2023-01-09 16:44:26 +0200

    everything fucking working smoothly

--- Function1.cs
+++ Function1.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 using Newtonsoft.Json;
@@ -11,30 +12,25 @@ using System.Threading.Tasks;
 
 namespace get_azuread_users
 {
-    public class Item
-    {
-        public int AzureUserQueryId { get; set; }
-        public DateTime? QueryDate { get; set; }
-        public string QueryResult { get; set; }
-    }
-
     public static class Function1
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log, [Sql("dbo.TR_AzureUserQuery", ConnectionStringSetting = "SqlConnectionString")] IAsyncCollector<Item> Items)
+            ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
             log.LogInformation(System.Environment.GetEnvironmentVariable("AZURE_CLIENT_ID", EnvironmentVariableTarget.Machine));
-            log.LogInformation(System.Environment.GetEnvironmentVariable("AZURE_CLIENT_ID", EnvironmentVariableTarget.User));
-            log.LogInformation(System.Environment.GetEnvironmentVariable("AZURE_CLIENT_ID", EnvironmentVariableTarget.Process));
+            log.LogInformation(System.Environment.GetEnvironmentVariable("AZURE_TENANT_ID", EnvironmentVariableTarget.Machine));
+            log.LogInformation(System.Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET", EnvironmentVariableTarget.Machine));
+            log.LogInformation(System.Environment.GetEnvironmentVariable("SqlConnectionString1", EnvironmentVariableTarget.Machine));
 
             // Values from app registration
             // https://learn.microsoft.com/en-us/graph/sdks/choose-authentication-providers?tabs=CS#client-credentials-provider
-            var clientId = System.Environment.GetEnvironmentVariable("AZURE_CLIENT_ID", EnvironmentVariableTarget.Process);
-            var tenantId = System.Environment.GetEnvironmentVariable("AZURE_TENANT_ID", EnvironmentVariableTarget.Process);
-            var clientSecret = System.Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET", EnvironmentVariableTarget.Process);
+            var clientId = System.Environment.GetEnvironmentVariable("AZURE_CLIENT_ID", EnvironmentVariableTarget.Machine);
+            var tenantId = System.Environment.GetEnvironmentVariable("AZURE_TENANT_ID", EnvironmentVariableTarget.Machine);
+            var clientSecret = System.Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET", EnvironmentVariableTarget.Machine);
+            var connectionString = System.Environment.GetEnvironmentVariable("SqlConnectionString1", EnvironmentVariableTarget.Machine);
 
             var options = new TokenCredentialOptions
             {
@@ -48,12 +44,26 @@ namespace get_azuread_users
             var jsonResult = new JsonResult(graphResult);
             var result = JsonConvert.SerializeObject(jsonResult.Value);
 
-            Item item = new() { QueryDate = DateTime.Now, QueryResult = result };
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    if (result != null)
+                    {
+                        var query = $"INSERT INTO [dbo].[TR_AzureUserQuery] (QueryDate,QueryResult) VALUES('{DateTime.Now}', '{result}')";
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                log.LogError(e.ToString());
+                return new BadRequestResult();
+            }
 
-            await Items.AddAsync(item);
-            await Items.FlushAsync();
-
-            return new JsonResult(item);
+            return new JsonResult(graphResult);
         }
     }
 }
